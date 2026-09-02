172310f4a2ea0a7f190012f5f9ce194edca52dd8
ada <ada@cubething.dev>
2026-09-01 22:27:46 -0500

    add jsut to path

--- netlify.sh
+++ netlify.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ./
-./just build
+curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ./bin/
+export PATH="$(pwd)/bin/:$PATH"
+just build
