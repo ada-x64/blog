3125f6cc2eb39d4b4205c926ea502954c1214d79
ada <ada@cubething.dev>
2026-09-02 13:36:32 -0500

    install fonts

--- netlify.sh
+++ netlify.sh
@@ -4,4 +4,13 @@ curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -
 export TYPST_INSTALL="./"
 curl -fsSL https://install.typst.community/install.sh | sh
 export PATH="$(pwd)/bin/:$PATH"
+
+FONT_HOME="$HOME/.local/share/fonts"
+echo "Installing fonts to $FONT_HOME"
+mkdir -p "$FONT_HOME"
+find ./resume/ ./src/static/fonts -type f \
+  \( -name '*.ttf' -o -name '*.otf' \) \
+  -exec cp -t "$FONT_HOME" {} +
+ls -l "$FONT_HOME"
+fc-cache -f -v
 just build
