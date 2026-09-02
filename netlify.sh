aef3d275154839abb791654c7709790126bb4fd2
ada <ada@cubething.dev>
2026-09-01 22:30:53 -0500

    install typst:

--- netlify.sh
+++ netlify.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ./bin/
+export TYPST_INSTALL="./bin"
+curl -fsSL https://install.typst.community/install.sh | sh
 export PATH="$(pwd)/bin/:$PATH"
 just build
