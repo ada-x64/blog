27effa962bc213b461fcdde28e6fa9f4b289544a
ada <ada@cubething.dev>
2026-09-05 13:52:26 -0500

    format scripts and style; use css vars

--- src/public/scripts/main.js
+++ src/public/scripts/main.js
@@ -1,28 +1,28 @@
 function rearrange_notes() {
-  const notes = document.querySelector(
-    'body > section[role="doc-endnotes"]'
-  );
-  const footer = document.querySelector('body > footer');
+	const notes = document.querySelector('body > section[role="doc-endnotes"]');
+	const footer = document.querySelector("body > footer");
 
-  if (notes && footer) {
-    footer.before(notes);
-  }
+	if (notes && footer) {
+		footer.before(notes);
+	}
 
-  const child = document.createElement("h2")
-  child.innerText = "Notes"
-  notes.querySelector("ol").before(child)
+	const child = document.createElement("h2");
+	child.innerText = "Notes";
+	notes.querySelector("ol").before(child);
 }
 
 let isDark = document.documentElement.classList.contains("dark");
 
 document.addEventListener("DOMContentLoaded", () => {
-  try {
-    rearrange_notes()
-  } catch {}
+	try {
+		rearrange_notes();
+	} catch {}
 
-  document.querySelector("#toggle-lights").addEventListener("click", (event) => {
-    isDark = !isDark;
-    document.querySelector("html").classList.toggle("dark")
-    window.localStorage.setItem("theme", isDark ? "dark" : "light");
-  })
-})
+	document
+		.querySelector(".toggle-lights")
+		.addEventListener("click", (_event) => {
+			isDark = !isDark;
+			document.querySelector("html").classList.toggle("dark");
+			window.localStorage.setItem("theme", isDark ? "dark" : "light");
+		});
+});
